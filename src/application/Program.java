6571c03422aa74460b9444e0175266338d77c890
Juvencio Linhares <juvlinhares@gmail.com>
2022-11-14 19:36:13 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -42,6 +42,12 @@ public class Program {
 		//pegar o id desse newSeller:
 		System.out.println("Inserted! New id = " + newSeller.getId());
 		
+		System.out.println("\n==== TEST 5: seller Update =====");
+		seller = sellerDao.findById(1);
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update Completed");
+		
 	}
 
 }
